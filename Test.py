11a6d97552fb26008fe132e71950e44ee589e104
aarhor <30049374+aarhor@users.noreply.github.com>
2025-04-18 23:06:45 +0200

    German => English

--- Test.py
+++ Test.py
@@ -10,15 +10,15 @@ list_Only_Folders = False
 
 with MailBox(imap_server).login(imap_username, imap_password) as MailBox:
     for g in MailBox.folder.list():
-        Ordner = g.name
+        Foldername = g.name
         
-        if Ordner not in folders_to_exclude:
-            print(Ordner)
+        if Foldername not in folders_to_exclude:
+            print(Foldername)
             if not list_Only_Folders:
-                if not os.path.exists(f"{imap_server}/{Ordner}"):
-                    os.makedirs(f"{imap_server}/{Ordner}")
+                if not os.path.exists(f"{imap_server}/{Foldername}"):
+                    os.makedirs(f"{imap_server}/{Foldername}")
 
-                MailBox.folder.set(Ordner)
+                MailBox.folder.set(Foldername)
                 for msg in MailBox.fetch(mark_seen=False):
                     uid = msg.uid
                     invalid_char = [
@@ -41,15 +41,15 @@ with MailBox(imap_server).login(imap_username, imap_password) as MailBox:
                         "\n",
                     ]
                     
-                    titel = msg.subject
-                    for zeichen in invalid_char:
-                        titel = titel.replace(zeichen, "_")
+                    Mail_Subject = msg.subject
+                    for char in invalid_char:
+                        Mail_Subject = Mail_Subject.replace(char, "_")
 
-                    DateiPfad = f"{imap_server}/{Ordner}/{uid}_{titel}.eml"
+                    FilePath = f"{imap_server}/{Foldername}/{uid}_{Mail_Subject}.eml"
                     
-                    if not os.path.exists(DateiPfad):
+                    if not os.path.exists(FilePath):
                         raw_email = msg.obj
-                        print(DateiPfad)
-                        with open(DateiPfad, "w", encoding="utf-8") as g:
+                        print(FilePath)
+                        with open(FilePath, "w", encoding="utf-8") as g:
                             with redirect_stdout(g):
                                 print(raw_email)
